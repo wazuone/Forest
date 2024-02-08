b6fa29b353238dfa55b71efef7e49aeb73bf1dc2
Mateusz <wzmateusz@gmail.com>
2024-02-08 20:32:23 +0100

    burger-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 document.body.style.background = 'grey'
+const nav = document.querySelector('.burger-nav')
+const navBtn = document.querySelector('.burger-btn')
+const burgerItems = document.querySelectorAll('.burger-item')
+
+const handleNav = () => {
+    nav.classList.toggle('active')
+
+    burgerItems.forEach( item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('active')
+        })
+    })
+
+    handleNavAnimation()
+}
+
+const handleNavAnimation = () => {
+    let delayTime = 0
+
+    burgerItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's'
+        delayTime++
+    })
+}
+
+navBtn.addEventListener('click', handleNav)
