fbce3e1831ab17f885037c386b423687e3d58c9b
Mateusz <wzmateusz@gmail.com>
2024-02-13 14:57:40 +0100

    header animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,33 @@ document.body.style.background = 'grey'
 const nav = document.querySelector('.burger-nav')
 const navBtn = document.querySelector('.burger-btn')
 const burgerItems = document.querySelectorAll('.burger-item')
+const headerText = document.querySelector('.header-text')
 
 const handleNav = () => {
-    nav.classList.toggle('active')
+	nav.classList.toggle('active')
 
-    burgerItems.forEach( item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('active')
-        })
-    })
+	burgerItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('active')
+		})
+	})
 
-    handleNavAnimation()
+	handleNavAnimation()
 }
 
 const handleNavAnimation = () => {
-    let delayTime = 0
+	let delayTime = 0
 
-    burgerItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's'
-        delayTime++
-    })
+	burgerItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
 }
+
+AOS.init({
+	startEvent: 'DOMContentLoaded',
+	once: true,
+})
 
 navBtn.addEventListener('click', handleNav)
